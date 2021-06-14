893f0a49ece7cd636799774bf187265c4c5de744
mpatsis <mpatsis13@gmail.com>
2021-06-14 18:28:00 +0100

    Some more cleaning for main file.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 	#Value Environments----------------------------------------
 	DATA_TYPE="Values"
 	env = gym.make('CartPole-v0')
+	
 	#Image env------------------------------------------------------
 	#DATA_TYPE="RGB_Image_RGB"
 	#DATA_TYPE="RGB_Image_GS"
@@ -23,11 +24,13 @@ if __name__ == '__main__':
 	#env = AtariPreprocessing(env, frame_skip=1)
 	#DATA_TYPE 
 	#---------------------------------------------------------------------
+	
 	state_dim = env.reset()
-	dtype, state_dim = format_data_type(state_dim,DATA_TYPE)
-	state_dim=state_dim.shape
+	dtype, state_dim = format_data_type(state_dim, DATA_TYPE)
+	state_dim = state_dim.shape
 	#print(dtype)
 	#env = AtariPreprocessing(env, frame_skip=1)
+	
 	"""[Variables]
 	"""
 	TRAIN_STEPS = 20
@@ -37,15 +40,18 @@ if __name__ == '__main__':
 	avg_score = 0
 	n_steps = 0
 	#env = gym.make('CartPole-v1')
-	agent = Agent(input_dims=4,output_dims=env.action_space.n,dtype=dtype,results=here)
+	agent = Agent(input_dims=4, output_dims=env.action_space.n, dtype=dtype, results=here)
+	
 	"""[Train ]
 	"""
 	env_name = env.unwrapped.spec.id
 	print(agent.get_agent_id())
 	agent_name = agent.get_agent_id()
-	figure_file = 'plots/'+env_name+'_'+agent_name+'.png'
+	figure_file = 'plots/' + env_name + '_' + agent_name + '.png'
 	best_score = env.reward_range[0]
 	score_history = []
+
+
 	for i in range(N_GAMES):
 		state=env.reset()
 		#print(env.observation_space,state.shape)
@@ -58,7 +64,7 @@ if __name__ == '__main__':
 			next_state, reward, done, info = env.step(action)
 			n_steps += 1
 			score += reward
-			agent.remember(i,state, next_state,action, action_log_prob,reward,value, done)
+			agent.remember(i, state, next_state, action, action_log_prob, reward, value, done)
 			if n_steps % TRAIN_STEPS == 0:
 				agent.learn_model()
 				learn_iters += 1
@@ -67,6 +73,6 @@ if __name__ == '__main__':
 		avg_score = np.mean(score_history[-100:])
 		if avg_score > best_score:
 			best_score = avg_score
-		print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,'time_steps', n_steps, 'learning_steps', learn_iters)
+		print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score, 'time_steps', n_steps, 'learning_steps', learn_iters)
 		x = [i+1 for i in range(len(score_history))]
 	plot_learning_curve(x, score_history, figure_file)
